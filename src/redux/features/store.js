5ee24d65fb44d4fcd9ceaa8b2048cbd1e0038e66
khemchand_saini <118979291+Khemchandsaini365@users.noreply.github.com>
2024-03-14 13:26:26 +0530

    Update store.js

--- src/redux/features/store.js
+++ src/redux/features/store.js
@@ -4,6 +4,7 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState: [],
   reducers: {
+    
     add:(state, action)=> {
         const productToAdd=action.payload;
       const existingProduct=state.find((item)=> item.id === productToAdd)
